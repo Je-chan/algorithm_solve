1dce893bad9d45b8ad45aaa4eb5758cd7bad3bb3
Je-chan <jkyyc3@gmail.com>
2023-08-06 00:00:11 +0900

    백준 11561, 좌표 정렬하기2- JS

---
+++ baek_joon/11651_좌표 정렬하기2/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('../dev/stdin').toString().trim().split('\n');
+
+console.log(
+  input
+    .slice(1)
+    .map((el) => el.split(' ').map(Number))
+    .sort((before, after) => before[1] - after[1] || before[0] - after[0])
+    .map((el) => el.join(' '))
+    .reduce((acc, cur) => acc + cur + '\n', '')
+);
