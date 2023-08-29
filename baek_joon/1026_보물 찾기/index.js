d0c7d1f9f1e719f4783faf15b5629bbf1d8cdca9
Je-chan <jkyyc3@gmail.com>
2023-08-30 00:23:51 +0900

    백준, 1026_보물 찾기 풀이

--- baek_joon/1026_보물 찾기/index.js
+++ baek_joon/1026_보물 찾기/index.js
@@ -2,25 +2,15 @@ const fs = require("fs");
 
 const input = fs.readFileSync("../dev/stdin").toString().trim().split("\n");
 
-const arrayGroup = input.slice(1).map((arr) => arr.split(" ").map(Number));
-const [sortedA, sortedB] = arrayGroup.map((arr, idx) =>
-  arr.sort((before, after) => (idx === 0 ? after - before : before - after))
+const [sortedA, sortedB] = input.slice(1).map((arr, idx) =>
+  arr
+    .split(" ")
+    .map(Number)
+    .sort((before, after) => (idx === 0 ? after - before : before - after))
 );
 
-const properMap = new Map();
-
-sortedB.forEach((el, idx) => {
-  if (!properMap.get(el)) {
-    properMap.set(el, [sortedA[idx]]);
-  } else {
-    properMap.set(el, [...properMap.get(el), sortedA[idx]]);
-  }
-});
-
-const test = sortedB.reduce((acc, cur, idx) => {
-  return acc + cur * sortedA[idx];
-});
-
-console.log(test);
-
-console.log(properMap);
+console.log(
+  sortedB.reduce((acc, cur, idx) => {
+    return acc + cur * sortedA[idx];
+  }, 0)
+);
