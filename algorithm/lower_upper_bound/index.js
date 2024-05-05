a4d1e218b052978b758d6ad335b41bea785586f3
ye chan <jkyyc3@gmail.com>
2024-05-05 22:45:39 +0900

    algorithm: 하한선, 상한선, 특정 원소 개수

---
+++ algorithm/lower_upper_bound/index.js
@@ -0,0 +1,31 @@
+// 하한선
+// : 정렬된 순서를 유지하면서 배열에 삽입할 가장 왼쪽 인덱스를 반환
+function lowerBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] >= target) {
+      end = mid; // 최대한 왼쪽으로 이동 (end 를 왼쪽으로 밀어주는 것)
+    } else start = mid + 1;
+  }
+
+  return end;
+}
+
+// 상한선
+// : 정렬된 순서를 유지하면서 배열에 삽입할 가장 오른쪽 인덱스를 반환
+function upperBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] > target) end = mid;
+    else start = mid + 1; // 최대한 오른쪽으로 이동 (start 를 오른쪽으로 밀어주는 것)
+  }
+
+  return end;
+}
+
+// 특정 원소의 개수
+// : 값이 [leftValue, rightValue] 인 데이터의 개수를 반환하는 함수
+function countByRange(arr, leftValue, rightValue) {
+  let rightIndex = upperBound(arr, rightValue, 0, arr.length);
+  let leftIndex = lowerBound(arr, leftValue, 0, arr.length);
+}
